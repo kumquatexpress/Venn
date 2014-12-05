0774f6a40d9f8259b4ce195f70898c164a767556
Boyang niu <boyang.niu@gmail.com>
2014-12-05 18:05:14 -0500

    styling of fb stuff, auth and creation of fb accounts

--- handlers/users_handler.py
+++ handlers/users_handler.py
@@ -1,5 +1,5 @@
-from flask import render_template, redirect, url_for, request
-from flask_login import login_user, flash
+from flask import render_template, redirect, url_for, request, make_response
+from flask_login import login_user, flash, logout_user
 import models
 import code
 from forms import Forms
@@ -21,8 +21,25 @@ def create():
         return redirect("/")
     return render_template("login.html", form=form, type="register")
 
+def facebook_create(info):
+    user = models.User.find_one({"facebook_uid": info["id"]})
+    if user is None:
+        user = models.User.insert({
+            "facebook_uid": info["id"],
+            "username": info["name"],
+            "gender": info["gender"]
+        })
+    if user is None:
+        return make_response(("Failed to create a user", 500, []))
+    login_user(user)
+    return make_response(("Logged in successfully", 200, []))
+
 def quiz():
     """get a question from db present it, get a form feedback
     send back as a response to db, win"""
     question = None
     return render_template("quiz.html", question=question)
+
+def logout():
+    logout_user()
+    return redirect("/")
