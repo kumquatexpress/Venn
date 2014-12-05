9e9fbb544a19902111c84c4a678ef3620d46dbce
Boyang niu <boyang.niu@gmail.com>
2014-12-05 18:05:14 -0500

    styling of fb stuff, auth and creation of fb accounts

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import login_required
 from handlers import main_handler, users_handler
 from models import User
+import code
 
 app = Blueprint('app', __name__, template_folder='templates')
 
@@ -17,10 +18,19 @@ def login():
 def register():
     return users_handler.create()
 
+@app.route("/facebook_auth", methods=["POST"])
+def facebook_auth():
+    return users_handler.facebook_create(request.json)
+
 @app.route("/users")
 @login_required
 def index_users():
     return users_handler.index()
+
+@app.route("/logout")
+@login_required
+def logout():
+    return users_handler.logout()
 
 @app.route("/quiz")
 @login_required
